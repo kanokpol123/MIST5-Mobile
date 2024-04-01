6a0ff9cba004f7c65e27d2f65107257642b4277e
kanokpol123 <kanokpolza32@gmail.com>
2024-04-01 18:56:30 +0700

    splash fix

--- mist5_mobile/lib/main.dart
+++ mist5_mobile/lib/main.dart
@@ -2,26 +2,31 @@ import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'api_constants.dart'; // Import the ApiConstants file
 import 'package:bcrypt/bcrypt.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 10),
+  );
+  FlutterNativeSplash.remove();
   // Call the method to fetch and print the sum of book IDs
   ApiConstants.countAll().then((count) {
     print('count of book IDs: $count');
   }).catchError((error) {
     print('Error fetching sum: $error');
   });
-  
+
   // Run the application
   runApp(LoginApp());
 }
-
-
 
 class LoginApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
+      title: 'MIS T5',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
